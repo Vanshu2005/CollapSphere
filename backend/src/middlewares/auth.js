9b73cea046fbf1c14a5e0b7b8967f1bdb8a6abee
Vanshika Agrawal <140275765+Vanshu2005@users.noreply.github.com>
2026-06-20 01:10:32 +0530

    Update  auth middleware
    Change default JWT secret to an empty string.

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -11,7 +11,7 @@ const userAuth = async (req, res, next) => {
         }
         
         // Validate the token 
-        const decodedObj = await jwt.verify(token, process.env.JWT_SECRET || "COOLAP@SPHERE");
+        const decodedObj = await jwt.verify(token, process.env.JWT_SECRET || "");
         const { _id } = decodedObj;
         
         // Find the user
